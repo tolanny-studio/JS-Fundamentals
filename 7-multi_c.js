07b48d680ea5d8475e498901b932f27ab7f9dd8c
tolanny-studio <hellotolannystudio@gmail.com>
2025-11-05 13:40:29 +0100

    bug fix for 7-multi_.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,9 @@
-const args = process.argv.slice(2);
-const value = args[0]
+#!/usr/bin/node
 
-  for(let i=0; i<value; i++){
-    console.log("C is fun");
-  }
+const x = parseInt(process.argv[2]);
 
-
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) console.log('C is fun');
+}
