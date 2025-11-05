8d8852b708bd8b547ef9a116b72b44387a8e6745
tolanny-studio <hellotolannystudio@gmail.com>
2025-11-05 13:44:43 +0100

    bug fix for 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,5 @@
-const statement = ["C is fun","Python is cool", "JavaScript is amazing"]
+#!/usr/bin/node
 
-for(let i =0; i < statement.length; i++){
-  console.log(statement[i]);
-  
-}
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lines.length; i++) console.log(lines[i]);
